c6522cec217e1bd2b4fa144e76e6b90de4bcf8d5
VinceEmond <vince.emond@icloud.com>
2022-02-17 22:28:23 +0000

    Removed test constant

--- breedFetcher.js
+++ breedFetcher.js
@@ -4,21 +4,19 @@ const request = require('request');
 const SEARCHARG = process.argv.slice(2);
 const WEBSITE = "https://api.thecatapi.com/v1/breeds/search?q=";
 
-
 const getBreedDescription = function(breed, callback) {
 
   request(WEBSITE + breed, (error, response, body)=> {
     
     if (error) {
-      callback(error);
+      callback(error,response);
     }
     
     let data = JSON.parse(body);
 
     // If data body exists, return the description via the callback function
     if (data[0]) {
-      callback(null,data[0].description);
-
+      callback(null,response,data[0].description);
     }
     
     // If it doesn't exist, return a message saying search was not succesful
@@ -31,9 +29,10 @@ const getBreedDescription = function(breed, callback) {
 };
 
 
-getBreedDescription(SEARCHARG,(error,description) =>{
+getBreedDescription(SEARCHARG,(error, response, description) =>{
   if (error) {
-    console.log("Error!", error);
+    console.log("Error: \n\n", error);
+    console.log("\n");
   } else {
     console.log(description);
   }
